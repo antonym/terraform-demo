39b77e5947c8a3256c0d8243867a8ab460890d40
Antony Messerli <antony@mes.ser.li>
2021-06-09 10:38:43 -0500

    Update versions

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.42.0"
+      version = "3.44.0"
     }
     google = {
       source  = "hashicorp/google"
-      version = "3.69.0"
+      version = "3.71.0"
     }
     openstack = {
       source  = "terraform-provider-openstack/openstack"
